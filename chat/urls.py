7f63ac73a9010fda2e8e7cdb4672f7bfb96ef9f8
Karts27 <kkharbanda9@gmail.com>
2021-07-26 22:45:27 +0530

    commit

---
+++ chat/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name = "homepage" ),
+    path("<str:room>/", views.room, name = "room"),
+    path("checkview", views.checkview, name = "checkview"),
+    path("send", views.send, name = "send"),
+    path("getMessages/<str:room>/", views.getMessages, name = "getMessages"),
+
+]
---
+++ chat/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from .models import Room, Message
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(room_name = room)
+    return render(request, 'room.html',
+    {
+        'username' : username,
+        'room' : room,
+        'room_details' : room_details
+    })
+
+
+def checkview(request):
+    room = request.POST['room_name']
+
+    username = request.POST['username']
+    print(room)
+
+    if Room.objects.filter(room_name = room).exists():
+        return redirect('/'+room+'/?username='+username)
+
+    else:
+        new_room = Room.objects.create(room_name = room)
+        new_room.save()
+        return redirect('/'+room+'/?username=' +username)
+
+
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message,
+                                         user = username,
+                                         room = room_id)
+    new_message.save()
+    return HttpResponse('Message Sent')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(room_name = room)
+    messages = Message.objects.filter(room = room_details.id)
+    return JsonResponse(
+        {
+        'messages' : list(messages.values())
+        }
+    )
